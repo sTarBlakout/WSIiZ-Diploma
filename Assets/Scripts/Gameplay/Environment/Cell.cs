5e447912adb40f46ef72ec7b6bd82d041c57a6d5
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2021-07-06 21:15:42 +0200

    Started with logic for adding neighbour cell in editor

--- Assets/Scripts/Gameplay/Environment/Cell.cs
+++ Assets/Scripts/Gameplay/Environment/Cell.cs
@@ -1,13 +1,36 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Gameplay.Environment
 {
     public class Cell : MonoBehaviour
     {
+        [SerializeField] private CellDirection newCellDirection;
+        
+        private (CellDirection direction, Cell cell)[] neighbourCells =
+        {
+            (CellDirection.N, null),
+            (CellDirection.S, null),
+            (CellDirection.NW, null),
+            (CellDirection.NE, null),
+            (CellDirection.SW, null),
+            (CellDirection.SE, null)
+        };
+        
         public List<Vector3> GetPathToCell()
         {
             return new List<Vector3> { transform.position };
+        }
+
+        [ContextMenu("Add Cell")]
+        public void AddCell()
+        {
+            var neighbour = neighbourCells.First(cell => cell.direction == newCellDirection);
+            if (neighbour.cell != null) return;
+            
+            // Add new cell here
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/Environment/EnvironmentEnums.cs
@@ -0,0 +1,7 @@
+
+namespace Gameplay.Environment
+{
+    public enum CellDirection { N, S, NW, NE, SW, SE }
+}
+
+
